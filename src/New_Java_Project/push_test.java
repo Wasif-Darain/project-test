214e2ec31ab2bbf56a86ec9faac1b1d0dd9f9206
Wasif Darain <wasifdarain@outlook.com>
2025-02-15 00:00:23 +0600

    This is another change.

--- src/New_Java_Project/push_test.java
+++ src/New_Java_Project/push_test.java
@@ -3,6 +3,6 @@ package New_Java_Project;
 public class push_test {
 	public static void main(String[] args)
 	{
-		System.out.println("Edit 2: Some changes to the code.");
+		System.out.println("Edit 4: Some amendments were made.");
 	}
 }
