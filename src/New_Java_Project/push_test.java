f5ec3639662c1e67e76fdd4adad3a11afde02acc
Wasif Darain <wasifdarain@outlook.com>
2025-02-15 00:02:50 +0600

    Added line: "Here we go again.".

--- src/New_Java_Project/push_test.java
+++ src/New_Java_Project/push_test.java
@@ -3,6 +3,7 @@ package New_Java_Project;
 public class push_test {
 	public static void main(String[] args)
 	{
-		System.out.println("Edit 4: Some amendments were made.");
+		System.out.println("Edit 5: Some amendments were made.");
+		System.out.println("Here we go again...");
 	}
 }
